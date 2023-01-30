39b45af7c7d4be3ecece176f590494ae8ecd20cf
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-01-31 01:05:59 +0900

    refactor : 사용자 입력 핸들러 상태 코드 400 -> 409로 변경

--- src/community/src/main/java/com/example/community/util/ExceptionController.java
+++ src/community/src/main/java/com/example/community/util/ExceptionController.java
@@ -1,6 +1,7 @@
 package com.example.community.util;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +13,6 @@ public class ExceptionController {
     // TODO: 상태코드 커스텀하여 수정
     @ExceptionHandler({IllegalArgumentException.class})
     public ResponseEntity BadInputException(IllegalArgumentException exception) {
-        return ResponseEntity.badRequest().body(exception.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
     }
 }
