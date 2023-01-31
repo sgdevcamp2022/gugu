55f36f7c2c2b6fb0d405f1c054acce0aef1b5e61
j8won <joowoni99@gmail.com>
2023-01-31 23:36:47 +0900

    feat: 서버 설정/사용자 관리/멤버 Container 마크업

---
+++ src/client/src/setting/server-member/ServerMemberContainer.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import styled from 'styled-components';
+import NavBar from './components/NavBar';
+import MemberInfoContainer from './components/MemberInfoContainer';
+
+const Container = styled.div`
+  flex: 1;
+`;
+
+const Heading3 = styled.div`
+  font-size: 1.3rem;
+  font-weight: 600;
+  color: ${(props) => props.theme.color.primaryText};
+`;
+
+const Divider = styled.div`
+  margin-top: 20px;
+  border-top: ${(props) => props.theme.border.primary};
+`;
+
+function ServerMemberContainer() {
+  const memberInfoArray = [
+    {
+      src: null,
+      name: 'blackbean99',
+      id: '@blackbean99#1231',
+      roleArray: [{ name: '백엔드', color: 'orange' }],
+    },
+    {
+      src: null,
+      name: '경주원',
+      id: '@경주원#1231',
+      roleArray: [{ name: '프론트엔드', color: 'green' }],
+    },
+    {
+      src: null,
+      name: '김정인',
+      id: '@주머니알#1231',
+      roleArray: [{ name: '백엔드', color: 'orange' }],
+    },
+    {
+      src: null,
+      name: '이윤성',
+      id: '@이윤성#1231',
+      roleArray: null,
+    },
+  ];
+
+  return (
+    <Container>
+      <Heading3>서버 멤버</Heading3>
+      <NavBar />
+      <Divider />
+      {memberInfoArray.map((info) => (
+        <MemberInfoContainer
+          key={info.name}
+          src={info.src}
+          name={info.name}
+          id={info.id}
+          roleArray={info.roleArray}
+        />
+      ))}
+    </Container>
+  );
+}
+
+export default ServerMemberContainer;
