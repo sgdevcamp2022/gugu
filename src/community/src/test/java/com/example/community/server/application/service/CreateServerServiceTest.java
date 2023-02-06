a7d11023de08fed7516606c0d89101d4ca051d4e
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-02-06 10:48:12 +0900

    test : 서버 생성 성공 단위테스트 추가

---
+++ src/community/src/test/java/com/example/community/server/application/service/CreateServerServiceTest.java
@@ -0,0 +1,21 @@
+package com.example.community.server.application.service;
+
+import com.example.community.server.application.port.in.CreateServerCommand;
+import com.example.community.server.application.port.out.RecordServerStatePort;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CreateServerServiceTest {
+    private final RecordServerStatePort recordServerStatePort = Mockito.mock(RecordServerStatePort.class);
+    private final CreateServerService createServerService = new CreateServerService(recordServerStatePort);
+
+    @Test
+    void isTrueServerIsCreatedByProperServerName() {
+        CreateServerCommand command = new CreateServerCommand("TestServer");
+        boolean success = createServerService.createServer(command);
+
+        assertThat(success).isTrue();
+    }
+}
