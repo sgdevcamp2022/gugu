494452aca3bf8c30366e9bd6242a61f56139f841
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-02-08 01:36:56 +0900

    refactor : id 타입 수정, Id가 있을 경우 객체 생성

--- src/community/src/main/java/com/example/community/server/domain/Server.java
+++ src/community/src/main/java/com/example/community/server/domain/Server.java
@@ -1,5 +1,6 @@
 package com.example.community.server.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,15 +10,20 @@ import javax.persistence.Id;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Server {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
     private String serverName;
     private String serverImage;
 
     public Server(String serverName, String serverImage) {
         this.serverName = serverName;
         this.serverImage = serverImage;
+    }
+
+    public static Server withId(int id, String serverName, String serverImage) {
+        return new Server(id, serverName, serverImage);
     }
 }
