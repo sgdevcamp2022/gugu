ce4361a66cb529e67cf2d7362d42ee3ee0ba4ad1
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-02-08 01:43:33 +0900

    feat : 서버 수정 API 추가

--- src/community/src/main/java/com/example/community/server/adapter/in/web/ServerController.java
+++ src/community/src/main/java/com/example/community/server/adapter/in/web/ServerController.java
@@ -1,8 +1,11 @@
 package com.example.community.server.adapter.in.web;
 
-import com.example.community.server.adapter.out.persistence.CreateServerRequestDto;
+import com.example.community.server.adapter.out.persistence.CreateServerDto;
+import com.example.community.server.adapter.out.persistence.ModifyServerDto;
 import com.example.community.server.application.port.in.CreateServerCommand;
 import com.example.community.server.application.port.in.CreateServerUseCase;
+import com.example.community.server.application.port.in.ModifyServerCommand;
+import com.example.community.server.application.port.in.ModifyServerUseCase;
 import com.example.community.util.ResultDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +19,7 @@ import java.net.URI;
 @RequiredArgsConstructor
 public class ServerController {
     private final CreateServerUseCase serverUseCase;
+    private final ModifyServerUseCase modifyServerUseCase;
 
     @PostMapping("/server")
     public ResponseEntity<ResultDto> createServer(@RequestBody CreateServerRequestDto createServer) {
@@ -28,6 +32,20 @@ public class ServerController {
                 .body(ResultDto.builder()
                         .code(201)
                         .message("서버 생성이 완료되었습니다.")
+                        .build());
+    }
+
+    @PatchMapping("/servers/{id}")
+    public ResponseEntity<ResultDto> updateServer(@PathVariable("id") int id, @RequestBody ModifyServerDto modifyServer) {
+        ModifyServerCommand command = new ModifyServerCommand(
+                modifyServer.getServerName(),
+                modifyServer.getImage()
+        );
+        modifyServerUseCase.modifyServer(id, command);
+        return ResponseEntity.ok()
+                .body(ResultDto.builder()
+                        .code(200)
+                        .message("서버 수정이 완료되었습니다.")
                         .build());
     }
 }
