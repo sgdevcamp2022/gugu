10b6c389e82828e4b1d8f2e4ed96f9e6fa2254ae
2yunseong <dbsdltjd3701@naver.com>
2023-02-07 22:13:06 +0900

    feat(media): mic/video on/off 로직 작성

--- src/client/src/media/Media.jsx
+++ src/client/src/media/Media.jsx
@@ -1,8 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/self-closing-comp */
 /* eslint-disable no-param-reassign */
 /* eslint-disable jsx-a11y/media-has-caption */
 /* eslint-disable no-undef */
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import styled from 'styled-components';
+import MicOff from './imgs/MicOff';
+import MicOn from './imgs/MicOn';
+import VideoOff from './imgs/VideoOff';
+import VideoOn from './imgs/VideoOn';
 
 const MyVideo = styled.video`
   border-radius: 10px;
@@ -10,11 +16,13 @@ const MyVideo = styled.video`
 
 function Media() {
   const myVideoRef = useRef();
-
+  const [micOn, setMicOn] = useState(true);
+  const [videoOn, setVideoOn] = useState(true);
+  let stream;
   useEffect(() => {
     const getUserMedia = async () => {
       try {
-        const stream = await window.navigator.mediaDevices.getUserMedia({
+        stream = await window.navigator.mediaDevices.getUserMedia({
           video: {
             width: 400,
             height: 400,
@@ -29,11 +37,29 @@ function Media() {
     getUserMedia();
   }, []);
 
+  const handleCameraBtn = () => {
+    stream.getVideoTracks().forEach((track) => {
+      track.enabled = !track.enabled;
+    });
+  };
+
+  const handleMicBtn = () => {
+    stream.getAudioTracks().forEach((track) => {
+      track.enabled = !track.enabled;
+    });
+  };
+
   return (
     <div>
       <div>
         <MyVideo ref={myVideoRef} autoPlay playsInline />
       </div>
+      <button onClick={handleCameraBtn} type="button">
+        {videoOn ? <VideoOn /> : <VideoOff />}
+      </button>
+      <button onClick={handleMicBtn} type="button">
+        {micOn ? <MicOn /> : <MicOff />}
+      </button>
     </div>
   );
 }
