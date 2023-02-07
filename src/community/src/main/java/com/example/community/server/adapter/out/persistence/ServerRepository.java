45ec3b0d3e63bd1e3897b284a961a306461140bb
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-02-08 01:41:50 +0900

    refactor : findById를 위해 id 타입을 DB와 같게 수정

--- src/community/src/main/java/com/example/community/server/adapter/out/persistence/ServerRepository.java
+++ src/community/src/main/java/com/example/community/server/adapter/out/persistence/ServerRepository.java
@@ -2,5 +2,5 @@ package com.example.community.server.adapter.out.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ServerRepository extends JpaRepository<ServerJpaEntity, Long> {
+public interface ServerRepository extends JpaRepository<ServerJpaEntity, Integer> {
 }
