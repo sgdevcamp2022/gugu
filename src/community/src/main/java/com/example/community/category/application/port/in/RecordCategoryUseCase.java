9cf7bfe76167601f38d2f4caba109a4140720b50
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-02-09 02:06:40 +0900

    feat : category 수정 서비스 추가

--- src/community/src/main/java/com/example/community/category/application/port/in/RecordCategoryUseCase.java
+++ src/community/src/main/java/com/example/community/category/application/port/in/RecordCategoryUseCase.java
@@ -2,4 +2,6 @@ package com.example.community.category.application.port.in;
 
 public interface RecordCategoryUseCase {
     boolean createCategory(Integer serverId, CreateCategoryCommand command);
+
+    boolean updateCategory(Integer serverId, UpdateCategoryCommand command);
 }
