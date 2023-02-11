f0815e70508d7524c3e346ad7a8a810f1a153590
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-02-12 00:29:53 +0900

    chores : 불필요한 공백 삭제

--- src/community/src/main/java/com/example/community/category/application/port/out/LoadCategoryStatePort.java
+++ src/community/src/main/java/com/example/community/category/application/port/out/LoadCategoryStatePort.java
@@ -1,6 +1,5 @@
 package com.example.community.category.application.port.out;
 
-import com.example.community.category.application.port.in.UpdateCategoryCommand;
 import com.example.community.category.domain.Category;
 
 public interface LoadCategoryStatePort {
--- src/community/src/main/java/com/example/community/category/application/service/RecordCategoryService.java
+++ src/community/src/main/java/com/example/community/category/application/service/RecordCategoryService.java
@@ -3,9 +3,7 @@ package com.example.community.category.application.service;
 import com.example.community.category.application.port.in.CreateCategoryCommand;
 import com.example.community.category.application.port.in.RecordCategoryUseCase;
 import com.example.community.category.application.port.in.UpdateCategoryCommand;
-import com.example.community.category.application.port.out.LoadCategoryStatePort;
 import com.example.community.category.application.port.out.RecordCategoryStatePort;
-import com.example.community.category.domain.Category;
 import com.example.community.server.application.port.out.LoadServerStatePort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,7 +12,6 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class RecordCategoryService implements RecordCategoryUseCase {
     private final RecordCategoryStatePort recordCategoryStatePort;
-    private final LoadCategoryStatePort loadCategoryStatePort;
     private final LoadServerStatePort loadServerStatePort;
 
     @Override
