3fb3e1bd8073dda44152427337289e0280136b8e
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-02-11 16:38:36 +0900

    feat : Channel 도메인 생성

---
+++ src/community/src/main/java/com/example/community/channel/domain/Channel.java
@@ -0,0 +1,37 @@
+package com.example.community.channel.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Channel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String channelName;
+    private Character channelType;
+    private Boolean isPrivate;
+    private String description;
+    private Integer serverId;
+    private Integer categoryId;
+
+    public static Channel withId(Integer id, String channelName, Character channelType, Boolean isPrivate, Integer serverId, Integer categoryId) {
+        return Channel.builder()
+                .id(id)
+                .channelName(channelName)
+                .channelType(channelType)
+                .isPrivate(isPrivate)
+                .serverId(serverId)
+                .categoryId(categoryId)
+                .build();
+    }
+}
