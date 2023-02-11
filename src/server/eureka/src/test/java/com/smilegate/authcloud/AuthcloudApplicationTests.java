780c7d3fbf04679d81a8a072f7fdd5772963aff3
이서현 <ymecca12@gmail.com>
2023-02-11 23:05:05 +0900

    refactor : multi module 배포형식으로 변경

---
+++ src/server/eureka/src/test/java/com/smilegate/authcloud/AuthcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smilegate.authcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
