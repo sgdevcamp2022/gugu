aff7abc34870ada0c36fa18185442615be1c5bf4
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-02-13 00:50:10 +0900

    refactor : Builder 패턴 사용

--- src/server/community/src/main/java/com/example/community/user/adapter/out/persistence/UserJpaEntity.java
+++ src/server/community/src/main/java/com/example/community/user/adapter/out/persistence/UserJpaEntity.java
@@ -1,5 +1,6 @@
 package com.example.community.user.adapter.out.persistence;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -36,13 +37,15 @@ public class UserJpaEntity {
     @Column
     private String message;
 
-    public UserJpaEntity(String email, String e_password, String userName, Date birth) {
+    @Builder
+    public UserJpaEntity(int user_id, String email, String e_password, String username, Date birth, String image, String banner_color, String message) {
+        this.user_id = user_id;
         this.email = email;
         this.e_password = e_password;
-        this.username = userName;
+        this.username = username;
         this.birth = birth;
-        this.image = "";
-        this.banner_color = "";
-        this.message = "";
+        this.image = image;
+        this.banner_color = banner_color;
+        this.message = message;
     }
 }
