3887311220e14c11e419a8e610e3829f59c67f3f
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-02-13 00:49:01 +0900

    refactor : validation 통한 검증을 위해 Date 입력타입 수정(Datetime -> String)

--- src/server/community/src/main/java/com/example/community/user/adapter/out/persistence/UserMapper.java
+++ src/server/community/src/main/java/com/example/community/user/adapter/out/persistence/UserMapper.java
@@ -3,9 +3,21 @@ package com.example.community.user.adapter.out.persistence;
 import com.example.community.user.domain.User;
 import org.springframework.stereotype.Component;
 
+import java.sql.Date;
+
 @Component
 class UserMapper {
     UserJpaEntity mapToJpaEntity(User user) {
-        return new UserJpaEntity(user.getEmail(), user.getPassword(), user.getUserName(), user.getBirth());
+        Date birth = Date.valueOf(user.getBirth());
+
+        return UserJpaEntity.builder()
+                .email(user.getEmail())
+                .e_password(user.getPassword())
+                .username(user.getUserName())
+                .birth(birth)
+                .image(user.getProfileImage())
+                .banner_color(user.getBannerColor())
+                .message(user.getMessage())
+                .build();
     }
 }
