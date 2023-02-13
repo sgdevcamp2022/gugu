6f22eb55ed68a4bfc975b67f4926112289c90063
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-02-13 22:07:44 +0900

    refactor : accessToken 반환 추가

--- src/server/community/src/main/java/com/example/community/user/adapter/out/persistence/TokenResponseDto.java
+++ src/server/community/src/main/java/com/example/community/user/adapter/out/persistence/TokenResponseDto.java
@@ -1,11 +1,14 @@
 package com.example.community.user.adapter.out.persistence;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 @AllArgsConstructor
-public class TokenResponse {
+public class TokenResponseDto {
+    private int code;
+    private String message;
     private String accessToken;
-    private String tokenType;
 }
