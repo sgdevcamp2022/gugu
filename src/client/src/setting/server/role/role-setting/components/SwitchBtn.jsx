1d0f7bea0e08283416009587124723e1ed5ce0a8
j8won <joowoni99@gmail.com>
2023-02-15 21:39:18 +0900

    feat: 권한 스위치 버튼 생성

---
+++ src/client/src/setting/server/role/role-setting/components/SwitchBtn.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { styled, Switch } from '@mui/material';
+import { PropTypes } from 'prop-types';
+
+const DiscordSwitch = styled((props) => (
+  // eslint-disable-next-line
+  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }) => ({
+  width: 40,
+  height: 24,
+  padding: 0,
+  '& .MuiSwitch-switchBase': {
+    padding: 0,
+    margin: 2,
+    transitionDuration: '300ms',
+    '&.Mui-checked': {
+      transform: 'translateX(16px)',
+      color: '#fff',
+      '& + .MuiSwitch-track': {
+        backgroundColor: '#3ba55c',
+        opacity: 1,
+        border: 0,
+      },
+      '&.Mui-disabled + .MuiSwitch-track': {
+        opacity: 0.5,
+      },
+    },
+    '&.Mui-focusVisible .MuiSwitch-thumb': {
+      color: '#33cf4d',
+      border: '6px solid #fff',
+    },
+    '&.Mui-disabled .MuiSwitch-thumb': {
+      color:
+        theme.palette.mode === 'light'
+          ? theme.palette.grey[100]
+          : theme.palette.grey[600],
+    },
+    '&.Mui-disabled + .MuiSwitch-track': {
+      opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    boxSizing: 'border-box',
+    width: 20,
+    height: 20,
+  },
+  '& .MuiSwitch-track': {
+    borderRadius: 26 / 2,
+    backgroundColor: '#72767d',
+    opacity: 1,
+    transition: theme.transitions.create(['background-color'], {
+      duration: 500,
+    }),
+  },
+}));
+
+function SwitchBtn({ checked, onChange }) {
+  return (
+    <div>
+      <DiscordSwitch
+        sx={{ m: 1 }}
+        checked={checked}
+        onChange={onChange}
+        inputProps={{ 'aria-label': 'switch' }}
+      />
+    </div>
+  );
+}
+
+SwitchBtn.propTypes = {
+  checked: PropTypes.bool,
+  onChange: PropTypes.func,
+};
+
+SwitchBtn.defaultProps = {
+  checked: false,
+  onChange: () => {},
+};
+
+export default SwitchBtn;
