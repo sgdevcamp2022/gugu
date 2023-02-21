a6c7c2776c8b8ca9160e50c236a138d3eaabfba1
j8won <joowoni99@gmail.com>
2023-02-21 19:29:04 +0900

    feat: 사용자 사이드바의 서버 아이콘 버튼 생성

---
+++ src/client/src/main/common/components/ServerIcon.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+import { PropTypes } from 'prop-types';
+import UserSideBarIcon from './UserSideBarIcon';
+
+const TextIcon = styled.div`
+  position: absolute;
+  z-index: 2;
+  color: white;
+`;
+
+const ImgIcon = styled.img`
+  object-fit: contain;
+`;
+
+function ServerIcon({ src, serverId, serverName }) {
+  return (
+    <UserSideBarIcon
+      onClick={() => {
+        console.log(serverName + serverId);
+      }}
+    >
+      {!src ? (
+        <TextIcon>{serverName}</TextIcon>
+      ) : (
+        <ImgIcon src={src} alt="server-profile" />
+      )}
+    </UserSideBarIcon>
+  );
+}
+
+ServerIcon.propTypes = {
+  src: PropTypes.string,
+  serverId: PropTypes.string.isRequired,
+  serverName: PropTypes.string,
+};
+
+ServerIcon.defaultProps = {
+  src: undefined,
+  serverName: '',
+};
+
+export default ServerIcon;
