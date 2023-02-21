73bf36b54c534a055169aafd0c2bd22227c59c11
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-02-21 20:50:22 +0900

    feat : 이미지 파일을 업로드하여 url로 변환함

---
+++ src/server/image/src/main/java/com/example/image/application/service/RecordImageService.java
@@ -0,0 +1,37 @@
+package com.example.image.application.service;
+
+import com.example.image.application.port.in.RecordImageUseCase;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Service
+public class RecordImageService implements RecordImageUseCase {
+
+    @Value("${file.server.path}")
+    private String uploadPath;
+
+    @Value("${file.upload.dir}")
+    private String uploadDir;
+
+
+    @Override
+    public String storeImage(MultipartFile image) {
+        String imageName = StringUtils.cleanPath(image.getOriginalFilename());
+        Path location = Paths.get(uploadDir + File.separator + imageName);
+        try {
+            Files.copy(image.getInputStream(), location, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException exception) {
+            throw new RuntimeException(exception.getMessage());
+        }
+        return uploadPath + imageName;
+    }
+}
